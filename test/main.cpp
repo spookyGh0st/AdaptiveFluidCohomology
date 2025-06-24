12ad9bb13637dffe8cedaa211999d63484b0f7f6
abtb0 <s74babt@uni-bonn.de>
2025-06-24 18:06:09 -0400

    rm tbb for now

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <gtest/gtest.h>
-#include <tbb/tbb.h>
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
@@ -12,27 +11,4 @@ TEST(HelloTest, BasicAssertions) {
 }
 
 
-using namespace oneapi::tbb;
 
-class ApplyFoo {
-    float *const my_a;
-public:
-    void operator()( const blocked_range<size_t>& r ) const {
-        float *a = my_a;
-        for( size_t i=r.begin(); i!=r.end(); ++i )
-            a[i] *= 2;
-    }
-    ApplyFoo( float a[] ) :
-        my_a(a)
-    {}
-};
-// Demonstrate some basic assertions.
-TEST(HelloTest, testTBB) {
-    std::vector<float> a( 1000000000);
-    parallel_for( blocked_range<size_t>(0,a.size()),
-    [=,&a](const blocked_range<size_t>& r) {
-        for(size_t i=r.begin(); i!=r.end(); ++i)
-            a[i] *= 2;
-    }
-);
-}
