44a8d05fc5c10cb7f58d55c36c300526f073fcf4
spookyghost <spookyghost@mailbox.org>
2025-07-11 16:20:06 +0200

    fix gradient implementation

--- eider/util.h
+++ eider/util.h
@@ -6,13 +6,26 @@
 
 namespace geometrycentral::surface
 {
+    /**
+     * Compute the gradient of a scalar function f over a triangle face by
+     * \( \nabla f = \frac{1}{2A} \sum_{i=0}^2 f_i \cdot R_{90}(e_i^*),\)
+     * where \( f_i \) is the value at vertex \( i \), \( e_i^* \) is the edge vector
+     * opposite vertex \( i \), \( R_{90} \) is a 90° counter-clockwise rotation,
+     * and \( A \) is the triangle area.
+     *
+     * @param geom Intrinsic geometry data, requires halfedgeVectorsInFace and faceAreas
+     * @param face The triangle face.
+     * @param f Scalar function on vertices.
+     * @return Gradient of f over the face.
+     */
     inline Vector2 grad(IntrinsicGeometryInterface& geom, Face face, const VertexData<double>& f)
     {
+        assert(face.isTriangle());
         Vector2 g = Vector2::zero();
         for (Halfedge he : face.adjacentHalfedges())
         {
             Vector2 efp = geom.halfedgeVectorsInFace[he.next()];
-            g += (-efp.rotate90()) * f[he.vertex()];
+            g += (efp.rotate90()) * f[he.vertex()];
         }
         return g / (2 * geom.faceAreas[face]);
     }
---
+++ test/utilTest.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+
+#include "eider/util.h"
+#include <filesystem>
+#include <geometrycentral/surface/meshio.h>
+
+using namespace geometrycentral;
+using namespace geometrycentral::surface;
+
+
+TEST(utilTest, testGradient){
+  std::filesystem::path fds(__FILE__);
+  fds = fds.parent_path()/ "models" /"grid.stl";
+  auto [m,g] = readManifoldSurfaceMesh(fds.string());
+  g->requireFaceTangentBasis();
+  g->requireHalfedgeVectorsInFace();
+  VertexData<double> f(*m);
+  for (Vertex v: m->vertices()) {
+    f[v] = g->vertexPositions[v].x;
+  }
+  for(Face face:m->faces()){
+     Vector2 gr = grad(*g,face,f);
+     Vector3 gr3d = g->faceTangentBasis[face][0] * gr[0] + g->faceTangentBasis[face][1] *gr[1];
+     ASSERT_NEAR(gr3d.x,1,1e-12);
+     ASSERT_NEAR(gr3d.y,0,1e-12);
+     ASSERT_NEAR(gr3d.z,0,1e-12);
+  }
+}
